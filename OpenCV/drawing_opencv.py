1d629ffbc8d708ecf054fee5fd1d29ad9599d8f8
Bartlomiej Szafran <bartekszafran@icloud.com>
2025-02-14 19:34:07 +0100

    update

--- OpenCV/drawing_opencv.py
+++ OpenCV/drawing_opencv.py
@@ -1,24 +1,34 @@
 import cv2
 import numpy as np
 
+#VAR
+drawing = False
+ix = -1
+iy = -1
 
 ######
-def draw_circle(event,x,y,flags,param):
+def draw_rectangle(event,x,y,flags,params):
+    global ix,iy, drawing
     
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y), 100, (255,0,0), -1)
+        drawing = True
+        ix, iy = x,y
+        
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(img,(ix,iy), (x,y),(0,255,0), -1)
     
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        cv2.circle(img,(x,y), 100, (0,255,0), -1)
-
-cv2.namedWindow(winname='my_drawing')
-cv2.setMouseCallback('my_drawing', draw_circle)
-
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img,(x,y))
+    
 #####
-img = np.zeros((512,512,3),np.uint8)
+img = np.zeros((512,512,3))
+cv2.namedWindow(winname='my_drawing_2')
+cv2.setMouseCallback('my_drawing_2', draw_rectangle)
+
 while True:
-    
-    cv2.imshow('my_drawing', img)
+    cv2.imshow('my_drawing_2', img)
     
     if cv2.waitKey(20) & 0xff == 27:
         break
